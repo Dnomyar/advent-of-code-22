b1b28023568134956e3d3bc1d6083d994eeae9a2
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2022-12-01 10:21:36 +0000

    day 1 part 1

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,8 +1,26 @@
+use std::fs;
+use std::str;
 
 
-pub fn dayone() -> u32 {
-    println!("Hello, world!");
-    return 3;
+fn read_file(fileName: &str)-> String {
+    return fs::read_to_string(fileName)
+        .expect("Unable to read the file");
+}
+
+fn sumCaloriesOfSingleElf(rawData: &str) -> u32{
+    return rawData
+    .split('\n')
+    .map(|calories| calories.parse::<u32>().unwrap())
+    .sum::<u32>();
+} 
+
+#[warn(dead_code)]
+pub fn dayone() -> Option<u32> {
+    return 
+        read_file("resources/day1.txt")
+        .split("\n\n")
+        .map(sumCaloriesOfSingleElf)
+        .max();
 }
 
 #[cfg(test)]
@@ -11,7 +29,7 @@ mod tests {
 
     #[test]
     fn day1_should_return_1(){
-        assert_eq!(dayone(), 1);
+        assert_eq!(dayone(), Some(67633));
     }
 
     
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 mod day_1;
-
 // pub use crate::advent_of_code::dayone;
-
 fn main(){
 }
 
