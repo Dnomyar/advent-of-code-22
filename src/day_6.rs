27e58f42edfa1911f09c3077580205a0bb7c09fc
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2022-12-06 16:25:24 +0000

    day 6

---
+++ src/day_6.rs
@@ -0,0 +1,74 @@
+use std::{collections::HashSet, fs, process::id, str::CharIndices};
+
+fn read_file(file_name: &str) -> String {
+    return fs::read_to_string(file_name).expect("Unable to read the file");
+}
+
+fn logic(input: &str, numberOfChar: usize) -> Result<usize, String> {
+    let charIndices: Vec<(usize, char)> = input.char_indices().collect();
+
+    let a: Vec<usize> = charIndices
+        .windows(numberOfChar)
+        .take_while(|window| {
+            let uniqueSetOfElements: HashSet<char> = window.into_iter().map(|a| a.1).collect();
+            return window.len() != uniqueSetOfElements.len();
+        })
+        .map(|window| {
+            return window[0].0;
+        })
+        .collect();
+
+    return match a.last() {
+        Some(idx) => Ok(*idx + numberOfChar + 1),
+        None => Err("could not find".to_string()),
+    };
+}
+
+pub fn part1(input: &str) -> Result<usize, String> {
+    return logic(input, 4);
+}
+
+pub fn part2(input: &str) -> Result<usize, String> {
+    return logic(input, 14);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bvwbjplbgvbhsrlpgdmjqwftvncz_should_return_5() {
+        let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+        assert_eq!(part1(input), Ok(5));
+    }
+
+    #[test]
+    fn nppdvjthqldpwncqszvftbrmjlhg_should_return_6() {
+        let input = "nppdvjthqldpwncqszvftbrmjlhg";
+        assert_eq!(part1(input), Ok(6));
+    }
+
+    #[test]
+    fn nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg_should_return_10() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        assert_eq!(part1(input), Ok(10));
+    }
+
+    #[test]
+    fn zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw_should_return_11() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        assert_eq!(part1(input), Ok(11));
+    }
+
+    #[test]
+    fn part_1() {
+        let input = read_file("resources/day6.txt");
+        assert_eq!(part1(&input), Ok(1287));
+    }
+
+    #[test]
+    fn part_2() {
+        let input = read_file("resources/day6.txt");
+        assert_eq!(part2(&input), Ok(3716));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_5;
+mod day_6;
 // pub use crate::advent_of_code::dayone;
 
 fn main() {}
