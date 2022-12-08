3cd282b87bcc1b8ceedc2fb9f5e166c1ffd48920
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2022-12-08 10:28:36 +0000

    Day 7 passing test exmaple dirty

--- src/day_7.rs
+++ src/day_7.rs
@@ -1,8 +1,11 @@
 use std::{
-    collections::{HashMap, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
+    f64::consts::E,
+    hash::Hash,
     io::ErrorKind,
 };
 
+use itertools::Itertools;
 use nom::{
     branch::alt,
     bytes::streaming::tag,
@@ -18,43 +21,56 @@ use nom::{
 enum Command {
     Cd { to: String },
     Dir { name: String },
-    File { name: String, size: u32 },
+    File { name: String, size: u64 },
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct ParentDir {
+    name: String,
+}
+
+impl TryFrom<FileType> for ParentDir {
+    type Error = String;
+
+    fn try_from(value: FileType) -> Result<Self, Self::Error> {
+        match value {
+            FileType::Dir { name } => Ok(ParentDir { name: name }),
+            _ => Err("is not a dir".to_string()),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+enum FileType {
+    Dir { name: String },
+    File { name: String, size: u64 },
+}
+trait FileTypeOps {
+    fn is_dir(&self) -> bool;
+    fn get_file_size(&self) -> Option<&u64>;
+}
+
+impl FileTypeOps for FileType {
+    fn is_dir(&self) -> bool {
+        match self {
+            FileType::Dir { name: _ } => true,
+            _ => false,
+        }
+    }
+
+    fn get_file_size(&self) -> Option<&u64> {
+        match self {
+            FileType::File { name, size } => Some(size),
+            _ => None,
+        }
+    }
 }
 
 fn not_line_ending2(input: &str) -> IResult<&str, &str> {
     not_line_ending(input)
 }
 
-pub fn part1(input: &str) -> Result<usize, String> {
-    let current_directory_deque: VecDeque<&str> = VecDeque::new();
-    let child_parent_relationship: HashMap<&str, &str> = HashMap::new();
-
-    /*
-    $ cd /
-    $ ls
-    dir a
-    14848514 b.txt
-    8504156 c.dat
-    dir d
-    $ cd a
-    $ ls
-    */
-    let input = "$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ ls
-584 i
-$ cd ..
-$ cd ..";
-
-    // let a = |e: &str| not_line_ending(e);
-    // let cd_to = map_res(not_line_ending2, |e: &str| -> Result<Command, &str> {
-    //     Ok(Command::Cd { to: e.to_string() })
-    // })(input);
-
+fn parseInput(input: &str) -> IResult<&str, Vec<Command>> {
     let cd = preceded(
         tag("$ cd "),
         map_res(not_line_ending2, |e: &str| -> Result<Vec<Command>, &str> {
@@ -63,7 +79,7 @@ $ cd ..";
     );
 
     let ls_options_dir = preceded(
-        tag("dir"),
+        tag("dir "),
         map_res(not_line_ending2, |e: &str| -> Result<Vec<Command>, &str> {
             Ok(Vec::from([Command::Dir {
                 name: e.to_string(),
@@ -72,7 +88,7 @@ $ cd ..";
     );
     let ls_option_file = map_res(
         tuple((
-            map_res(digit1, |s: &str| s.parse::<u32>()),
+            map_res(digit1, |s: &str| s.parse::<u64>()),
             char(' '),
             not_line_ending2,
         )),
@@ -94,20 +110,120 @@ $ cd ..";
 
     let options = alt((cd, ls));
 
-    let res = separated_list1(char('\n'), options)(input);
-    // // let res = cd.map(|(to, _)| Command::Cd { to: to.to_string() });
-    println!("RES = {:?}", res);
+    return map_res(separated_list1(char('\n'), options), |commands| {
+        Ok::<Vec<Command>, &str>(commands.into_iter().flatten().collect())
+    })(input);
+}
 
-    // let cd = tag("$ cd")(input);
+pub fn part1(input: &str) -> Result<u64, String> {
+    println!("helle");
 
-    // separated_list1(char("", f)
+    let mut current_directory_deque: VecDeque<String> = VecDeque::new();
+    let mut child_parent_relationship: HashMap<FileType, ParentDir> = HashMap::new();
+    let commands = parseInput(input).unwrap().1;
 
-    // tag("$ cd ").map(|a| a);
-    // alt(
-    //     tag("$ cd ").map(|a|a),
-    //     // map_res(s)
-    // )
-    return todo!();
+    println!("commands {:?}", commands);
+
+    for command in commands {
+        match command {
+            Command::Cd { to } => {
+                if to == ".." {
+                    current_directory_deque.pop_front();
+                } else {
+                    current_directory_deque.push_front(to);
+                }
+            }
+            Command::Dir { name } => {
+                let parent_dir = ParentDir {
+                    name: current_directory_deque.front().unwrap().to_string(),
+                };
+                child_parent_relationship.insert(FileType::Dir { name: name }, parent_dir);
+            }
+            Command::File { name, size } => {
+                let parent_dir = ParentDir {
+                    name: current_directory_deque.front().unwrap().to_string(),
+                };
+                child_parent_relationship.insert(
+                    FileType::File {
+                        name: name,
+                        size: size,
+                    },
+                    parent_dir,
+                );
+            }
+        }
+    }
+
+    println!("map {:?}", child_parent_relationship);
+
+    let graph: HashMap<ParentDir, HashSet<FileType>> = child_parent_relationship
+        .clone()
+        .into_iter()
+        .into_group_map_by(|a| a.1.clone())
+        .into_iter()
+        .map(|(k, v)| {
+            (
+                k,
+                v.into_iter()
+                    .map(|(fileType, _)| fileType)
+                    .collect::<HashSet<FileType>>(),
+            )
+        })
+        .collect();
+
+    println!("");
+    graph.clone().into_iter().for_each(|e| println!("{:?}", e));
+    println!("");
+
+    let mut edges_to_visit = VecDeque::from([ParentDir {
+        name: "/".to_string(),
+    }]);
+
+    let mut directory_sizes: HashMap<ParentDir, u64> = HashMap::new();
+    println!("starting");
+
+    while !edges_to_visit.is_empty() {
+        let front = edges_to_visit.front().unwrap();
+        let children = graph.get(front).unwrap(); //.unwrap_or(&empty);
+        let children_directories: HashSet<_> = children
+            .into_iter()
+            .flat_map(|d| match d {
+                FileType::Dir { name } => Some(ParentDir {
+                    name: name.to_string(),
+                }),
+                _ => None,
+            })
+            .filter(|child| !directory_sizes.contains_key(child))
+            .collect();
+
+        if children_directories.is_empty() {
+            let total_size: u64 = children
+                .into_iter()
+                .flat_map(|child| match child {
+                    FileType::Dir { name } => directory_sizes.get(&ParentDir {
+                        name: name.to_string(),
+                    }),
+                    file @ FileType::File { name, size } => child.get_file_size(),
+                })
+                .sum();
+            directory_sizes.insert(front.clone(), total_size);
+            // res = Some(total_size);
+            edges_to_visit.pop_front();
+        } else {
+            children_directories
+                .into_iter()
+                .for_each(|directory| edges_to_visit.push_front(directory));
+        }
+    }
+    println!("res {:?}", directory_sizes);
+
+    let sum_small_directories: u64 = directory_sizes
+        .into_iter()
+        .filter(|(k, v)| v < &100000)
+        .map(|(k, v)| v)
+        .sum();
+
+    return Ok(sum_small_directories);
 }
 
 pub fn part2(input: &str) -> Result<usize, String> {
@@ -128,28 +244,28 @@ mod tests {
     #[test]
     fn test() {
         let input = "$ cd /
-        $ ls
-        dir a
-        14848514 b.txt
-        8504156 c.dat
-        dir d
-        $ cd a
-        $ ls
-        dir e
-        29116 f
-        2557 g
-        62596 h.lst
-        $ cd e
-        $ ls
-        584 i
-        $ cd ..
-        $ cd ..
-        $ cd d
-        $ ls
-        4060174 j
-        8033020 d.log
-        5626152 d.ext
-        7214296 k";
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k";
         assert_eq!(part1(input), Ok(95437));
     }
 }
